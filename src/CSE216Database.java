ab1bcca3a4c6e34f38771587319117ca6e788949
Toribelle <39226984+Toribelle@users.noreply.github.com>
2020-11-20 18:23:39 +0800

    Update CSE216Database.java

--- src/CSE216Database.java
+++ src/CSE216Database.java
@@ -1,3 +1,15 @@
 public class CSE216Database {
+   public String getUsername() {
+       return Username;
+   
+   public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+       return password;
+    }
+    public void setPassword(String password) {
+      this.password = password;
+ }
    
 }
