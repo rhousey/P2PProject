3044084d0c1333fa2258831c6461891978c07e92
Michael Fitzgerald <mtf323@lehigh.edu>
2020-11-20 15:40:36 -0500

    Implemented crypto system (MAX. 63 chars, not including null char)

--- src/CSE216Crypto.java
+++ src/CSE216Crypto.java
@@ -17,12 +17,42 @@
  * @author Bill Phillips
  *
  */
+
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
 public class CSE216Crypto 
 {
    private String tStr = " ";
    
-   public void encrypt(String s){ tStr = s; } 
-   public void decrypt(String s){ tStr = s; }
+   private BigInteger p = new BigInteger(
+ 		  "23973131022914996346071393858876827486275304524239358526904124826518948010013");
+   private BigInteger q = new BigInteger(
+ 		  "47682209267361752906439234498452546764741727765730925307147436953319593264669");
+   private BigInteger n = new BigInteger(
+ 		  "11430918502285149779499046340017015199914441182466056855548973758723371013526"
+ 		  + "03883289432913006822644664798203011634939049630050313222214071696535571130697");
+   private BigInteger tot = new BigInteger(
+ 		  "28577296255712874448747615850042537999786102956165142138872434396808427533813"
+ 		  + "3056987285659064392533509110218409345980504335020007347040627479174257464004");
+   
+   private BigInteger e = new BigInteger("65537");
+   private BigInteger d = e.modInverse(tot);
+   
+   public void encrypt(String s) { 
+	  BigInteger m = new BigInteger(s.getBytes(Charset.defaultCharset()));
+	  BigInteger c = m.modPow(e, n);
+	      
+      tStr = c.toString();
+   }
+   
+   public void decrypt(String s) { 
+	  BigInteger c = new BigInteger(s);
+      BigInteger m = c.modPow(d, n);
+      
+      tStr = new String(m.toByteArray(), Charset.defaultCharset());
+   }
+   
    public String getMsg()    { return tStr; }
    
 }
--- src/CSE216Database.java
+++ src/CSE216Database.java
@@ -1,15 +1,18 @@
 public class CSE216Database {
+    
+   String username;
+   String password;
+	
    public String getUsername() {
-       return username;
-   
+      return username;
+   }
    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-       return password;
-    }
-    public void setPassword(String password) {
+      this.username = username;
+   }
+   public String getPassword() {
+      return password;
+   }
+   public void setPassword(String password) {
       this.password = password;
- }
-   
+   }
 }
