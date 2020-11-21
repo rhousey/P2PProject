1789b79e2caf207ffc0cd1278ac1324b715f3463
rhousey <houseyrebecca8@gmail.com>
2020-11-20 19:50:03 -0500

    Menu

--- src/CSE216ClientCommandLineInterface.java
+++ src/CSE216ClientCommandLineInterface.java
@@ -1,18 +1,3 @@
-import java.util.*;
-
-/** <p>Title: P2P Project</p>
- *
- * <p>Description: This class gets a test message.       
- * </p>
- *
- * <p>Copyright: none</p>
- *
- * <p>Company: Lehigh University</p>
- *
- * @author Bill Phillips 
- *
- */
-
 public class CSE216ClientCommandLineInterface {
    
    private final RunTimeVars rtv = 
@@ -36,11 +21,19 @@ public class CSE216ClientCommandLineInterface {
               (final String hname, final String m) 
    {
 
-      prompt = "Enter a string: ";
+      prompt = "Host: " + hostname
+              +        "     Node ID: " + m + "\n"
+              + "1) Add Grade\n"
+              + "2)  Change Grade \n"
+              + "3)  Calculate Average Student Grade\n"  
+              + "4) Calcuate Gradebook Class Average\n"; 
       
-      sc =                 new Scanner(System.in);
       
+      sc = new Scanner(System.in);
+     
    }
+      
+     
    
    //
    // This function prompts
@@ -57,11 +50,43 @@ public class CSE216ClientCommandLineInterface {
       cs.mid    = MessageID.MSG;
       String enteredString = "abcdefg";
       
-      System.out.print (prompt);
+      System.out.print(prompt);
+       try {
+             sel = sc.nextInt();
+       }
+      catch(Exception e){
+                  System.exit(0);
+      }
 
+         
+         if(sel > 4 || sel < 0)
+      {
+         do{
+         System.out.println("Invalid input, Please choose from one of the menu items.");  
+          System.out.print(prompt);
+         }
+         while(sel > 4 || sel < 0 ); 
+      }
+         
+       if(sel == 1){ 
+          cs.setV(1);
+       }
+       if(sel == 2){
+          cs.setV(2); 
+       }
+       if(sel == 3){ 
+          cs.setV(3); 
+       } 
+       
+       System.out.println("Enter the students name.");
+       
+       if(sel == 4){ 
+          cs.setV(4); 
+       }
+       
       // Get the string from the user //
       enteredString = sc.nextLine();
- 
+      
       // If the string is "quit"
       if ("quit".equals(enteredString))
          System.exit(0);
